87cd6fec36a7e6c308983bab7f73fdda667e2fee
Samir Ghosh <hello@samir.tech>
2021-06-23 13:37:21 -0700

    trying keyup

--- test.py
+++ test.py
@@ -9,17 +9,16 @@ win = pygame.display.set_mode((100,100))
 
 while True:
    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-      if event.type == pygame.KEYDOWN:
-        key = event.key
-        if key == pygame.K_a:
-            kit.motor1.throttle = -0.8
-        elif key == pygame.K_d:
-            kit.motor1.throttle = 0.8
-        else:
+        if event.type == pygame.KEYDOWN:
+            key = event.key
+            if key == pygame.K_a:
+                kit.motor1.throttle = -0.8
+            elif key == pygame.K_d:
+                kit.motor1.throttle = 0.8
+        if event.type == pygame.KEYUP:
             kit.motor1.throttle = 0.0
-        
             
              
