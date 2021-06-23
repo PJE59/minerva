5c5484fe37e1f845c61f9cb6e69f64770ac59dee
Samir Ghosh <hello@samir.tech>
2021-06-23 13:07:30 -0700

    avoid try catch block

--- test.py
+++ test.py
@@ -3,12 +3,18 @@ import keyboard  # using module keyboard
 kit = MotorKit()
 
 while True:
-    try:
-        if keyboard.is_pressed('a'):
-            kit.motor1.throttle = -1.0
-        elif keyboard.is_pressed('d'):
-            kit.motor1.throttle = 1.0
-        else:
-            kit.motor1.throttle = 0
-    except:
-        break
+    # try:
+    #     if keyboard.is_pressed('a'):
+    #         kit.motor1.throttle = -1.0
+    #     elif keyboard.is_pressed('d'):
+    #         kit.motor1.throttle = 1.0
+    #     else:
+    #         kit.motor1.throttle = 0
+    # except:
+    #     break
+    if keyboard.is_pressed('a'):
+        kit.motor1.throttle = -1.0
+    elif keyboard.is_pressed('d'):
+        kit.motor1.throttle = 1.0
+    else:
+        kit.motor1.throttle = 0
